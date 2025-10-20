f1530818b979fbae41b684f173ab1b23c00231dd
nucleargezi <604223110@qq.com>
2025-10-21 01:52:38 +0800

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blockcuttest.netlify.app/"
+  site: "https://blockcuttest.netlify.app/",
+  integrations: [preact()]
 });
